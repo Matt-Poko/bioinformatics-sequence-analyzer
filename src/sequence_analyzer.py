41d7bca85cb59c3a69c31c81f300d49022861528
Matt-Poko <matt.pokornicki@gmail.com>
2026-01-15 18:47:04 -0500

    Removed runner logic from SequenceAnalyzer class

--- src/sequence_analyzer.py
+++ src/sequence_analyzer.py
@@ -4,8 +4,6 @@ from collections import Counter
 import matplotlib.pyplot as plt 
 import re
 import random
-import argparse
-import sys
 
 class SequenceAnalyzer:
     def __init__(self, file_path):
@@ -133,37 +131,3 @@ class SequenceAnalyzer:
         return new_seq
     
         
-       
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="Sequence Analyzer", description="Analyzes gene sequences from fasta files")
-    parser.add_argument('--input', required=True, help="Path to desired fasta file")
-    parser.add_argument('--operation', required=True, 
-                        help='Operation to perform on the fasta file data. Available options[CODON-USEAGE, GET-CODONS, TRANSLATE, MUTATE-POINT, MUTATE-RANDOM, GRAPH-GC]')
-    args = parser.parse_args()
-    analyzer = SequenceAnalyzer(args.input)
-    operation = args.operation
-    match operation:
-       case 'CODON-USAGE':
-          print(analyzer.codon_usage())  
-       case 'GET-CODONS':
-          print('Enter a {Sequence ID}')
-          sequence_id = input()
-          print(analyzer.get_codons(sequence_id))
-       case 'TRANSLATE':
-          print(analyzer.dna_to_protein())
-       case 'MUTATE-POINT':
-          print('Enter a {Sequence ID}, {Position}, and {New Base}')
-          input = input()
-          func_params = input.split(" ")
-          print(analyzer.point_mutation(func_params[0], int(func_params[1]), func_params[2]))
-       case 'MUTATE-RANDOM':
-          print('Enter a {Sequence ID}')
-          input = input()
-          func_params = input.split(" ")
-          print(analyzer.simulate_random_mutation(func_params[0]))
-       case 'GRAPH-GC':
-          print('Enter a {File Name}')
-          file_name = input()
-          analyzer.plot_gc_content(file_name)
-       case _:
-          sys.exit("Invalid operation Please ensure operation matches casing. Terminating")
